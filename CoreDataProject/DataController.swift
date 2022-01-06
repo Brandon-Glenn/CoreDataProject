5a9a5aabe79bef200c32d9d5022119baff2dcff6
Brandon Glenn <Brandon.Glenn@icloud.com>
2022-01-06 08:31:07 -0700

    Implement many to one merge policy

--- CoreDataProject/DataController.swift
+++ CoreDataProject/DataController.swift
@@ -13,6 +13,7 @@ class DataController: ObservableObject {
     let container = NSPersistentContainer(name: "CoreDataProject")
     
     init() {
+        print("Initialize DataController")
         container.loadPersistentStores { description, error in
             if let error = error {
                 print ("Core data failed to load: \(error.localizedDescription)")
